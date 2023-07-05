28ec64eeb3b226448e7c3f87cd51007946e53a4d
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-07-05 17:57:59 -0400

    Properties are hard

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,32 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    
+    def __init__(self, name = 'Person', job = "job"):
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if type(name) == type('string') and 0 < len(name) < 26:
+            self._name = name.title()
+            return self._name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            
+    name = property(get_name, set_name)
+
+    def get_job(self):
+        return self._job
+
+    def set_job(self, job):
+        cased_jobs = [job.lower() for job in APPROVED_JOBS]
+        if job.lower() in cased_jobs:
+            self._job = job
+            return self._job
+        else:
+            print("Job must be in list of approved jobs.")
+        
+    job = property(get_job, set_job)
